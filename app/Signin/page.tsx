157c3c0bb8bb16b99f1cc6635894b9bef23755f4
ashele1234 <calebashele@gmail.com>
2025-08-24 13:50:20 -0300

    fixed export errors

--- app/Signin/page.tsx
+++ app/Signin/page.tsx
@@ -13,7 +13,7 @@ import { Eye, EyeOff, Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 
-export const signInWithEmail = async (email:string, password: string) => {
+const signInWithEmail = async (email:string, password: string) => {
   return await signInWithEmailAndPassword(auth, email, password);
 };
 
